b4c7441c62879adc9269769210b0b50ae5db4086
Jonas Aschenbrenner <jonas.aschenbrenner@gmail.com>
2021-09-04 12:46:15 +0200

    Wip

---
+++ set_wind.py
@@ -0,0 +1,16 @@
+from pymem import Pymem
+
+from main import GunboundProcess
+
+
+def main():
+    process = GunboundProcess(Pymem('GunBound.gme'))
+    wind_direction = 0  # in degrees
+    wind_speed = 22
+    while True:
+        process.set_wind_direction(wind_direction)
+        process.set_wind_speed(wind_speed)
+
+
+if __name__ == '__main__':
+    main()
