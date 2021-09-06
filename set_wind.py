04dffce8c7c49cb828a476f299943087c98d1b42
Jonas Aschenbrenner <jonas.aschenbrenner@gmail.com>
2021-09-06 12:13:38 +0200

    Boomer hook shot (WiP)

--- set_wind.py
+++ set_wind.py
@@ -6,7 +6,7 @@ from main import GunboundProcess
 def main():
     process = GunboundProcess(Pymem('GunBound.gme'))
     wind_direction = 0  # in degrees
-    wind_speed = 0
+    wind_speed = 2
     while True:
         process.set_wind_direction(wind_direction)
         process.set_wind_speed(wind_speed)
